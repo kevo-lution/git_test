9df39105c3406afce147efe137c73627f1b6ed4a
Kevin Richardson <kevinr@brettrobinson.com>
2025-02-18 12:47:45 -0600

    added buttons

--- rps.js
+++ rps.js
@@ -1,6 +1,6 @@
 // players
 let player1;
-let cpu;
+let player2;
 //options
 const rock = 0;
 const paper = 1;
@@ -10,9 +10,7 @@ const scissors = 2;
 let player1score = 0;
 let player2score = 0;
 let targetScore = 0;
-let winner = null;
 
-//playerLogic
 function playerLogic() {
     let selection = Number(prompt("Enter a number 0 - 2: Rock = 0, Paper = 1, Scissors = 2"));
     return selection; // Convert the input to a number
@@ -22,20 +20,6 @@ function playerLogic() {
 function computerLogic() {
     let decision = Math.floor(Math.random() * 3); // Random number between 0 and 2
     return decision;
-}
-
-//js displaying on html
-let cpuScore = document.getElementById("cpuScore");
-let p1Score = document.getElementById("p1Score");
-
-function updateWinner(){
-	if (winner === player1score){
-		player1score++;
-		document.getElementById("p1Score").textContent = player1score
-	} else if(winner === player2score){
-		player2score++
-		document.getElementById("cpuScore").textContent = player2score
-	}
 }
 
 
@@ -49,49 +33,53 @@ function playRound(targetScore){
 			break;
 		}
 		let player1 = playerLogic();
-		let cpu = computerLogic();
+		let player2 = computerLogic();
 		console.log(`Player 1 chose ${player1 == 0 ? 'Rock' : player1 == 1 ? 'Paper' : 'Scissors'}`);
-		console.log(`The computer chose ${cpu == 0 ? 'Rock' : cpu == 1 ? 'Paper' : 'Scissors'}`);
+		console.log(`The computer chose ${player2 == 0 ? 'Rock' : player2 == 1 ? 'Paper' : 'Scissors'}`);
 
-	
-		if (player1 === rock && cpu === paper) {
-			console.log(`CPU wins! Paper beats rock`);
-			updateWinner(winner = player2score)
-			console.log(`Current score\n Player 1: ${player1score}\n CPU: ${player2score}`)
+		
+
+		if (player1 === rock && player2 === paper) {
+			console.log(`Player 2 wins! Paper beats rock`);
+			player2score++;
+			console.log(`Current score\n Player1: ${player1score}\n Player2: ${player2score}`)
 		} 
-		else if (player1 === paper && cpu === rock) {
+		else if (player1 === paper && player2 === rock) {
 			console.log(`Player 1 wins! Paper beats rock`);
-			updateWinner(winner = player1score)
-			console.log(`Current score\n Player 1: ${player1score}\n CPU: ${player2score}`)
+			player1score++;
+			console.log(`Current score\n Player1: ${player1score}\n Player2: ${player2score}`)
 		} 
-		else if (player1 === paper && cpu === scissors) {
-			console.log(`CPU wins! Scissors beats paper`);
-			updateWinner(winner = player2score);
-			console.log(`Current score\n Player 1: ${player1score}\n CPU: ${player2score}`)
+		else if (player1 === paper && player2 === scissors) {
+			console.log(`Player 2 wins! Scissors beats paper`);
+			player2score++
+			console.log(`Current score\n Player1: ${player1score}\n Player2: ${player2score}`)
 		} 
-		else if (player1 === scissors && cpu === paper) {
+		else if (player1 === scissors && player2 === paper) {
 			console.log(`Player 1 wins! Scissors beats paper`);
-			updateWinner(winner = player1score)
-			console.log(`Current score\n Player 1: ${player1score}\n CPU: ${player2score}`)
+			player1score++
+			console.log(`Current score\n Player1: ${player1score}\n Player2: ${player2score}`)
 		} 
-		else if (player1 === scissors && cpu === rock) {
-			console.log(`CPU wins! Rock beats scissors`);
-			updateWinner(winner = player2score);
-			console.log(`Current score\n Player 1: ${player1score}\n CPU: ${player2score}`)
+		else if (player1 === scissors && player2 === rock) {
+			console.log(`Player 2 wins! Rock beats scissors`);
+			player2score++
+			console.log(`Current score\n Player1: ${player1score}\n Player2: ${player2score}`)
 		} 
-		else if (player1 === rock && cpu === scissors) {
+		else if (player1 === rock && player2 === scissors) {
 			console.log(`Player 1 wins! Rock beats scissors`);
-			updateWinner(winner = player1score);
-			console.log(`Current score\n Player 1: ${player1score}\n CPU: ${player2score}`)
+			player1score++
+			console.log(`Current score\n Player1: ${player1score}\n Player2: ${player2score}`)
 		} 
-		else if (player1 === cpu) {
+		else if (player1 === player2) {
 			console.log("It's a tie!");
-			console.log(`Current score\n Player 1: ${player1score}\n CPU: ${player2score}`)
+			console.log(`Current score\n Player1: ${player1score}\n Player2: ${player2score}`)
 		}
 	}
 }
 
-//playRound(3);
+// modal to ask how many rounds to play.
+// edit computerLogic to sleep a float anywhere between 0 - 1.5 seconds before giving an answer
 
-
-//variable for cpu/player score and link it with getElementByID
+/* make rock, paper, and scissors a button
+which initiates a round of r,p,s 
+show the winner of the round on screen and update their score
+*/
